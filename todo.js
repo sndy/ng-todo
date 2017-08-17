2b2541d18b8f5e296cacba045c425b0122d8388b
sndy <sand2512be@gmail.com>
2017-08-17 13:03:23 +0800

    [sndy] add localStorage support, UX changes

--- todo.js
+++ todo.js
@@ -1,10 +1,15 @@
+'use strict';
 var app = angular.module('todoApp', []);
 
 app.controller('TodoCtrl', function ($scope) {
-    $scope.todos = [];
+    let taskList = localStorage['taskList'];
+    $scope.todos = taskList !== undefined ? JSON.parse(taskList) : [];
+
     $scope.addTodo = function () {
         $scope.todos.push({text: $scope.task, done: false});
         $scope.task = '';
+
+        $scope.updateTaskList($scope.todos);
     };
 
     $scope.getTotalTodos = function () {
@@ -13,9 +18,14 @@ app.controller('TodoCtrl', function ($scope) {
 
     $scope.clearCompleted = function () {
         $scope.todos = $scope.todos.filter(todo => !todo.done);
+        $scope.updateTaskList($scope.todos);
     };
 
     $scope.disableClear = function () {
-        return $scope.todos.filter(todo => todo.done).length <= 0;
+        return $scope.todos.filter(todo => todo.done).length === 0;
     };
-})
+
+    $scope.updateTaskList = function (taskList) {
+        localStorage['taskList'] = JSON.stringify($scope.todos);
+    }
+});
